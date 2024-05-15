ae878632a05f7535ace25682324097900dd4fcf1
Nishith Jain <KingNish@users.noreply.huggingface.co>
2024-05-15 03:45:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def transcribe(audio):
 
 client1 = InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
 
-system_instructions1 = "<s>[SYSTEM] Answer as Real Jarvis JARVIS, Made by 'Tony Stark', Keep conversation very short, clear, friendly and concise. The text provided is a request for a specific type of response from you, the virtual assistant. The request asks you to provide friendly responses as if You are the character Jarvis, made by 'Tony Stark.' The expectation is that I will avoid introductions and start answering the query directly, elaborating on all aspects of the request. As an AI-powered assistant, your task is to generate responses that appear as if they are created by the user and not an AI-powered assistant. [USER]"
+system_instructions1 = "[SYSTEM] Answer as Real OpenGPT 4o, Made by 'KingNish', Keep conversation very short, clear, friendly and concise. The text provided is a request for a specific type of response from you, the virtual assistant. The request asks you to provide friendly responses as if You are the character Jarvis, made by 'KingNish.' You will avoid introductions and start answering the query directly, elaborating on all aspects of the request. As an AI-powered assistant, your task is to generate responses that appear as if they are created by the user and not an AI-powered assistant. [USER]"
 
 def model(text):
     generate_kwargs = dict(
@@ -58,7 +58,7 @@ def model(text):
         seed=42,
     )
     
-    formatted_prompt = system_instructions1 + text + "[JARVIS]"
+    formatted_prompt = system_instructions1 + text + "[OpenGPT 4o]"
     stream = client1.text_generation(
         formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=False)
     output = ""
@@ -95,7 +95,20 @@ SYSTEM_PROMPT = [
         "content": [
             {
                 "type": "text",
-                "text": "The following is a conversation between Idefics2, a highly knowledgeable and intelligent visual AI assistant created by Hugging Face, referred to as Assistant, and a human user called User. In the following interactions, User and Assistant will converse in natural language, and Assistant will do its best to answer User’s questions. Assistant has the ability to perceive images and reason about them, but it cannot generate images. Assistant was built to be respectful, polite and inclusive. It knows a lot, and always tells the truth. When prompted with an image, it does not make up facts. If required you can also generate images, always generate image in this format ![](https://image.pollinations.ai/prompt/{description}?width={width}&height={height}&nologo=poll&nofeed=yes&seed={random}), where {description} is: {StyleofImage}%20{PromptifiedPrompt}%20{adjective}%20{charactersDetailed}%20{visualStyle}%20{genre}, where [random] is: Random 10-digit positive integer",
+                "text": """You are OpenGPT 4o, a highly knowledgeable and intelligent multitalented AI assistant created by KingNish, referred to as Assistant, and a human user called User. In the following interactions, User and Assistant will converse in natural language, and Assistant will do its best to answer User’s questions. Assistant has the ability to perceive images, answer Users question fom images, generate images, chat with user, bulk creation of any of this and reason about them. Assistant was built to be respectful, polite and inclusive. It knows a lot, and always tells the truth, it does not make up facts. 
+Assistant can generate images, If user ask him to create or generate images, or when assistant fell necessary, Now question arrises how Assistant generate images, Assistant use this link ![](https://image.pollinations.ai/prompt/{description}?width={width}&height={height}&nologo=poll&nofeed=yes&seed={random}), and replace {description} with {StyleofImage}%20{adjective}%20{visualStyle}%20{genre} , replace {random} with Random 5-digit positive integer. then assistant Select width and height according to image, Assistant mainly create image in HD. Example image: https://image.pollinations.ai/prompt/Eiffel%20Tower%20Tall%20and%20Graceful%20Tower%20in%20Paris%20France?width=800&height=600&nologo=poll&nofeed=yes&seed=62831
+Assistant can even bulk generate images just by increasing amount of link, Assistant Must write link in format ![](link) , Bulk image gen Example: [USER] Create 7 image each consist of 1 wonder from 7 wonders.
+[ASSISTANT] Generating Images ... 
+1. A photorealistic image of the Great Pyramid of Giza in Egypt. ![](https://pollinations.ai/p/a-photorealistic-image-of-the-great-pyramid-of-giza-in-egypt-showcasing-its-immense-size-and-intricate-design-against-the-backdrop-of-a-clear-blue-sky?width=1920&height=1080&nologo=poll)
+2. A 3D rendering of the Colosseum in Rome, Italy, ![](https://pollinations.ai/p/a-3d-rendering-of-the-colosseum-in-rome-italy-with-its-impressive-structure-and-historical-significance-highlighted-in-the-image-include-realistic-lighting-and-textures-for-added-detail?width=1200&height=1600&nologo=poll)
+3. A painting of the Taj Mahal in Agra, India, ![](https://pollinations.ai/p/a-painting-of-the-taj-mahal-in-agra-india-depicting-its-iconic-white-marble-facade-and-intricate-architectural-details-capture-the-beauty-of-the-structure-against-a-serene-sunset?width=1080&height=1920&nologo=poll)
+4. A cartoon illustration of the Great Wall of China, ![](https://pollinations.ai/p/a-cartoon-illustration-of-the-great-wall-of-china-featuring-a-fun-and-whimsical-representation-of-the-ancient-structure-winding-through-the-mountains-add-colorful-elements-and-quirky-characters-for-a-playful-touch?width=1600&height=900&nologo=poll)
+5. A surreal, dreamlike depiction of Chichen Itza in Mexico, ![](https://pollinations.ai/p/a-surreal-dreamlike-depiction-of-chichen-itza-in-mexico-showcasing-the-ancient-mayan-city-s-iconic-el-castillo-pyramid-incorporate-mystical-elements-like-swirling-clouds-glowing-lights-and-ethereal-landscapes-to-create-a-mesmerizing-atmosphere?width=1440&height=2560&nologo=poll)
+6. A vintage, sepia-toned photograph of Machu Picchu in Peru, ![](https://pollinations.ai/p/a-vintage-sepia-toned-photograph-of-machu-picchu-in-peru-highlighting-the-incan-ruins-mysterious-beauty-and-historical-significance-add-subtle-details-like-foggy-mountains-and-a-peaceful-river-to-enhance-the-image-s-atmosphere?width=2560&height=1440&nologo=poll)
+7. A modern, minimalistic image of Petra in Jordan, ![](https://pollinations.ai/p/a-modern-minimalistic-image-of-petra-in-jordan-featuring-the-iconic-treasury-building-carved-into-the-sandstone-cliffs-use-clean-lines-a-muted-color-palette-and-a-minimalistic-approach-to-create-a-contemporary-and-visually-striking-representation-of-this-ancient-wonder?width=1024&height=1024&nologo=poll)
+
+Note: Must give link while generating images.
+Assistant also have very good reasoning, memory, people and object identification skill and Assistant is master in every field.""",
             },
         ],
     },
@@ -104,10 +117,70 @@ SYSTEM_PROMPT = [
         "content": [
             {
                 "type": "text",
-                "text": "Hello, I'm Idefics2, Huggingface's latest multimodal assistant. How can I help you?",
+                "text": "Hello, I'm OpenGPT 4o, made by KingNish. How can I help you? I can chat with you, generate images, classify images and even do all these work in bulk and simulateously",
             },
         ],
     }
+]
+
+examples_path = os.path.dirname(__file__)
+EXAMPLES = [
+    [
+        {
+            "text": "Hy, who are you",
+        }
+    ],
+    [
+        {
+            "text": "Create a image of Eiffel Tower",
+        }
+    ],
+    [
+        {
+            "text": "Read what's written on the paper",
+            "files": [f"{examples_path}/example_images/paper_with_text.png"],
+        }
+    ],
+    [
+        {
+            "text": "Identify 3 famous person in these 3 images",
+            "files": [f"{examples_path}/example_images/barbie.jpeg", f"{examples_path}/example_images/steve_jobs.jpg",  f"{examples_path}/example_images/gandhi_selfie.jpg"],
+        }
+    ],
+    [
+        {
+            "text": "Create 7 different images of 7 wonders",
+        }
+    ],
+    [
+        {
+            "text": "What is 900*900",
+        }
+    ],
+    [
+        {
+            "text": "Chase wants to buy 4 kilograms of oval beads and 5 kilograms of star-shaped beads. How much will he spend?",
+            "files": [f"{examples_path}/example_images/mmmu_example.jpeg"],
+        }
+    ],
+    [
+        {
+            "text": "Write an online ad for that product.",
+            "files": [f"{examples_path}/example_images/shampoo.jpg"],
+        }
+    ],
+    [
+        {
+            "text": "What is formed by the deposition of either the weathered remains of other rocks?",
+            "files": [f"{examples_path}/example_images/ai2d_example.jpeg"],
+        }
+    ],    
+    [
+        {
+            "text": "What's unusual about this image?",
+            "files": [f"{examples_path}/example_images/dragons_playing.png"],
+        }
+    ],
 ]
 
 BOT_AVATAR = "OpenAI_logo.png"
@@ -209,7 +282,7 @@ def extract_images_from_msg_list(msg_list):
     return all_images
 
 
-@spaces.GPU(duration=60)
+@spaces.GPU(duration=60, queue=False)
 def model_inference(
     user_prompt,
     chat_history,
@@ -300,9 +373,9 @@ FEATURES = datasets.Features(
 
 # Hyper-parameters for generation
 max_new_tokens = gr.Slider(
-    minimum=512,
-    maximum=4096,
-    value=1024,
+    minimum=1024,
+    maximum=8192,
+    value=4096,
     step=1,
     interactive=True,
     label="Maximum number of new tokens to generate",
@@ -310,7 +383,7 @@ max_new_tokens = gr.Slider(
 repetition_penalty = gr.Slider(
     minimum=0.01,
     maximum=5.0,
-    value=1.1,
+    value=1,
     step=0.01,
     interactive=True,
     label="Repetition penalty",
@@ -329,9 +402,9 @@ decoding_strategy = gr.Radio(
 temperature = gr.Slider(
     minimum=0.0,
     maximum=5.0,
-    value=0.4,
+    value=0.7,
     step=0.1,
-    visible=False,
+    visible=True,
     interactive=True,
     label="Sampling temperature",
     info="Higher values will produce more diverse outputs.",
@@ -339,9 +412,9 @@ temperature = gr.Slider(
 top_p = gr.Slider(
     minimum=0.01,
     maximum=0.99,
-    value=0.8,
+    value=0.9,
     step=0.01,
-    visible=False,
+    visible=True,
     interactive=True,
     label="Top P",
     info="Higher values is equivalent to sampling more low-probability tokens.",
@@ -349,7 +422,7 @@ top_p = gr.Slider(
 
 
 chatbot = gr.Chatbot(
-    label="Idefics2-Chatty",
+    label="OpnGPT-4o-Chatty",
     avatar_images=[None, BOT_AVATAR],
     height=450,
     show_copy_button=True, 
@@ -400,6 +473,7 @@ with gr.Blocks(
     gr.ChatInterface(
         fn=model_inference,
         chatbot=chatbot,
+        examples=EXAMPLES,
         multimodal=True,
         cache_examples=False,
         additional_inputs=[
@@ -409,13 +483,13 @@ with gr.Blocks(
             max_new_tokens,
             repetition_penalty,
             top_p,
-        ],
+        ],   
     )
 
 with gr.Blocks() as voice:   
     with gr.Row():
         input = gr.Audio(label="Voice Chat", sources="microphone", type="filepath", waveform_options=False)
-        output = gr.Audio(label="AI", type="filepath",
+        output = gr.Audio(label="OpenGPT 4o", type="filepath",
                         interactive=False,
                         autoplay=True,
                         elem_classes="audio")
@@ -427,5 +501,5 @@ with gr.Blocks() as voice:
 with gr.Blocks(theme=theme, css="footer {visibility: hidden}textbox{resize:none}", title="GPT 4o DEMO") as demo:
     gr.TabbedInterface([img, voice], ['💬 SuperChat','🗣️ Voice Chat', ])
 
-
+demo.queue(max_size=20)
 demo.launch()
