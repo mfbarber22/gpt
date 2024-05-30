cc897a22954765f24bd93ef3907ff78349635219
Nishith Jain <KingNish@users.noreply.huggingface.co>
2024-05-30 15:54:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -566,7 +566,7 @@ with gr.Blocks() as instant:
     gr.HTML("<iframe src='https://kingnish-sdxl-flash.hf.space' width='100%' height='2000px' style='border-radius: 8px;'></iframe>")
 
 with gr.Blocks() as dalle:
-    gr.HTML("<iframe src='https://ehristoforu-dalle-3-xl-lora-v2.hf.space' width='100%' height='2000px' style='border-radius: 8px;'></iframe>")
+    gr.HTML("<iframe src='https://kingnish-image-gen-pro.hf.space' width='100%' height='2000px' style='border-radius: 8px;'></iframe>")
 
 with gr.Blocks() as image:
     gr.Markdown("""### More models are coming""")
