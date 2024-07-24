1c3216f814edab63cc9003f7b15472e43b3c93a4
Nishith Jain <KingNish@users.noreply.huggingface.co>
2024-07-24 16:03:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,41 @@
 import gradio as gr
 
 # Import modules from other files
-from chatbot import chatbot, model_inference, EXAMPLES
-from live_chat import videochat
+from chatbot import model_inference, EXAMPLES, chatbot
+from voice_chat import respond
 
 # Define Gradio theme
 theme = gr.themes.Soft(
-    primary_hue="blue",
-    secondary_hue="orange",
+    primary_hue="sky",
+    secondary_hue="violet",
     neutral_hue="gray",
-    font=[gr.themes.GoogleFont('Libre Franklin'), gr.themes.GoogleFont('Public Sans'), 'system-ui', 'sans-serif']
-).set(
-    body_background_fill_dark="#111111",
-    block_background_fill_dark="#111111",
-    block_border_width="1px",
-    block_title_background_fill_dark="#1e1c26",
-    input_background_fill_dark="#292733",
-    button_secondary_background_fill_dark="#24212b",
-    border_color_primary_dark="#343140",
-    background_fill_secondary_dark="#111111",
-    color_accent_soft_dark="transparent"
+    font=[gr.themes.GoogleFont('orbitron')]
 )
 
 
-with gr.Blocks() as voice:    
-    gr.Markdown("## Temproraly Not Working (Update in Progress)")
+# Create Gradio blocks for different functionalities
 
 # Chat interface block
 with gr.Blocks(
-        fill_height=True,
         css=""".gradio-container .avatar-container {height: 40px width: 40px !important;} #duplicate-button {margin: auto; color: white; background: #f1a139; border-radius: 100vh; margin-top: 2px; margin-bottom: 2px;}""",
 ) as chat:
-    gr.Markdown("### Chat with Image, Chat with Video, Image Generation and Normal Chat")
+    gr.Markdown("### Image Chat, Image Generation, Image classification and Normal Chat")
     gr.ChatInterface(
         fn=model_inference,
-        chatbot=chatbot,
+        chatbot = chatbot,
         examples=EXAMPLES,
         multimodal=True,
         cache_examples=False,
-        additional_inputs=[
-            gr.Checkbox(label="Web Search", value=False),
-        ],
-    )    
-
-# Live chat block
-with gr.Blocks() as livechat:
-    gr.Interface(
-        fn=videochat,
-        inputs=[gr.Image(type="pil",sources="webcam", label="Upload Image"), gr.Textbox(label="Prompt", value="what he is doing")],
-        outputs=gr.Textbox(label="Answer")
+        autofocus=False,
+        concurrency_limit=10,
     )
+
+# Voice chat block
+with gr.Blocks() as voice:
+    with gr.Row():
+        audio_input = gr.Audio(label="Voice Chat (BETA)", sources="microphone", type="filepath", waveform_options=False)
+        output = gr.Audio(label="JARVIS", type="filepath", interactive=False, autoplay=True, elem_classes="audio")
+    audio_input.change( fn=respond, inputs=[audio_input], outputs=[output], queue=False)
 
 with gr.Blocks() as image:
     gr.HTML("<iframe src='https://kingnish-image-gen-pro.hf.space' width='100%' height='2000px' style='border-radius: 8px;'></iframe>")
@@ -59,12 +45,12 @@ with gr.Blocks() as instant2:
 
 with gr.Blocks() as video:
     gr.Markdown("""More Models are coming""")
-    gr.TabbedInterface([ instant2], ['Instant🎥'])   
+    gr.TabbedInterface([ instant2], ['Instant🎥'])     
 
 # Main application block
 with gr.Blocks(theme=theme, title="OpenGPT 4o DEMO") as demo:
-    gr.Markdown("# OpenGPT 4o\n### Try its small,fast and unlimited version from here: [OpenGPT 4o Mini](https://huggingface.co/spaces/KingNish/OpenGPT-4o-mini)")
-    gr.TabbedInterface([chat, voice, livechat, image, video], ['💬 SuperChat','🗣️ Voice Chat','📸 Live Chat', '🖼️ Image Engine', '🎥 Video Engine'])
+    gr.Markdown("# OpenGPT 4o")
+    gr.TabbedInterface([chat, voice, image, video], ['💬 SuperChat','🗣️ Voice Chat', '🖼️ Image Engine', '🎥 Video Engine'])
 
 demo.queue(max_size=300)
 demo.launch()
